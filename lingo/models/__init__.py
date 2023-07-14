ff508ad2fdc06968bc5492ab1389e05aac00487a
wengsyx <wengsyx@gmail.com>
2023-07-14 17:09:26 +0800

    Update Lingo to v1.1.4

--- lingo/models/__init__.py
+++ lingo/models/__init__.py
@@ -288,7 +288,7 @@ def get_model_and_tokenizer(args):
                 # Load checkpoint
                 load_checkpoint(model, args)
 
-                if args.quantization_bit is not None and not args.from_quantized_checkpoint:
+                if args.quantization_bit and not args.from_quantized_checkpoint:
                     # Quantize model before moving to GPU
                     model = quantize(model, args.quantization_bit, lora=args.use_lora)
 
